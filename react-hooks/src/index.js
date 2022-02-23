bb820476d23e79d8bc1bd9e488fb1a89e0fd28c1
rajat <rkumar201@sapient.com>
2022-02-22 21:46:36 -0600

    form using

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,42 +1,23 @@
-import React, { useState, useReducer } from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const initialState = {
-  message: "hi"
-};
-
-function reducer(state, action) {
-  switch(action.type) {
-    case "yell":
-      return {
-        message: "HEY!"
-      };
-    case "whisper":
-      return {
-        message: "excuse me"
-      };
-    default: {
-      return {
-        message: "nothing"
-      }
-    }
-  }
-}
-
 function App() {
 
-  const [state, dispatch ] = useReducer(
-    reducer,
-    initialState
-  );
+  const sound = useRef()
+  const color = useRef()
 
   return (
-    <>
-      <p>Message: {state.message}</p>
-      <button onClick={ () => dispatch({type: "yell"}) }>YELL</button>
-      <button onClick={ () => dispatch({type: "whisper"}) }>Whisper</button>
-    </>
+    <form>
+      <input 
+        ref={sound}
+        type="text"
+        placeholder='Sound...' />
+      <input 
+        ref={color}
+        type="color" />
+      <button>ADD</button>
+    </form>
   );
 }
 
